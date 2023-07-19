d7335906d1b2629b8c779524db935b1caff176f7
Nathanael Sensfelder <spamshield0@noot-noot.org>
2023-07-19 21:59:52 +0200

    This should fix the regex node issues.

--- run_example.sh
+++ run_example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPLY_RATE=2
-REGEX_CAPTURE='^(?P<req>\?RL?R?) (?P<pre>(\S+[ ]+)*)(?P<name>(nr|nightred|tase)[^ \n]*)(?P<post>[ ]?.*)'
+REGEX_CAPTURE="^(?P<req>\?RL?R?) (?P<pre>(\S+[ ]+)*)(?P<name>(nr|nightred|tase)[^ \n]*)(?P<post>[ ]?.*)"
 REGEX_REPLACE='?RR \g<pre>\g<post>'
 REPLACE_NAMES_WITH="USERNAME"
 MARKOV_ORDER=3
@@ -21,7 +21,7 @@ function start_cmd
 
 start_cmd "jh-server" "$JH_DIR/server/markov-k-disk/run.sh /tmp/jh0 $MARKOV_ORDER $DATABASE_DIR"
 start_cmd "jh-limiter" "$JH_DIR/filter/limiter/run.sh /tmp/lt0 /tmp/jh0 $REPLY_RATE"
-start_cmd "jh-regex" "$JH_DIR/filter/regex/run.sh /tmp/rx0 /tmp/jh0 /tmp/lt0 $REGEX_CAPTURE $REGEX_REPLACE"
+start_cmd "jh-regex" "$JH_DIR/filter/regex/run.sh /tmp/rx0 /tmp/jh0 /tmp/lt0 '$REGEX_CAPTURE' '$REGEX_REPLACE'"
 start_cmd "jh-lowercase" "$JH_DIR/filter/lowercase/run.sh /tmp/lc0 /tmp/twr0"
 start_cmd "jh-two-way-replace" "$JH_DIR/filter/two-way-replace/run.sh /tmp/twr0 /tmp/rx0"
 start_cmd "jh-discord" "$JH_DIR/gateway/discord/run.sh /tmp/pers0 $DISCORD_KEY"
